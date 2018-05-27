88d15855c32a789682027b02f3418338c639bf30
Juan Camilo Sandoval Devia <juansandoval@rccl.com>
2018-05-27 18:32:41 -0500

    Changes in the architecture, dependencies in a module for more modularity.

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,90 @@
+object ApplicationId {
+    val application_id = "com.zemogaposttest.juansandoval.zemogaposttest"
+}
+
+object Modules {
+    val domain = ":domain"
+    val data = ":data"
+}
+
+object Releases {
+    val version_code = 1
+    val version_name = "1.0"
+}
+
+object Versions {
+    val gradle = "3.1.2"
+
+    val versions = "0.17.0"
+
+    val compile_sdk = 27
+    val min_sdk = 21
+    val target_sdk = 27
+
+    val inject = "1"
+
+    val support = "27.0.2"
+    val maps = "12.0.0"
+
+    val kotlin = "1.2.31"
+    val anko = "0.10.4"
+    val timber = "4.7.0"
+    val rxandroid = "2.0.2"
+    val rxkotlin = "2.2.0"
+    val retrofit = "2.4.0"
+    val logging_interceptor = "3.10.0"
+    val dagger = "2.15"
+    val glide = "4.6.1"
+    val rxpaper = "1.2.0"
+    val moshi = "1.4.0"
+    val lifecycle = "1.1.1"
+
+    val junit = "4.12"
+    val assertj_core = "3.9.1"
+    val mockito_kotlin = "1.5.0"
+}
+
+object Libraries {
+    val inject = "javax.inject:javax.inject:${Versions.inject}"
+
+    val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    val anko_commons = "org.jetbrains.anko:anko-commons:${Versions.anko}"
+
+    val maps = "com.google.android.gms:play-services-maps:${Versions.maps}"
+
+    val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+
+    val rxandroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxandroid}"
+    val rxkotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlin}"
+
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    val rxjava_adapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    val moshi_converter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
+    val logging_interceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.logging_interceptor}"
+
+    val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    val dagger_compiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+
+    val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    val glide_compiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+
+    val rxpaper = "com.github.pakoito:RxPaper2:${Versions.rxpaper}"
+    val moshi = "com.squareup.moshi:moshi:${Versions.moshi}"
+
+    val lifecycle_extensions = "android.arch.lifecycle:extensions:${Versions.lifecycle}"
+    val lifecycle_compiler = "android.arch.lifecycle:compiler:${Versions.lifecycle}"
+}
+
+object SupportLibraries {
+    val appcompat_v7 = "com.android.support:appcompat-v7:${Versions.support}}"
+    val design = "com.android.support:design:${Versions.support}"
+    val cardview_v7 = "com.android.support:cardview-v7:${Versions.support}"
+    val recyclerview_v7 = "com.android.support:recyclerview-v7:${Versions.support}"
+}
+
+object TestLibraries {
+    val junit = "junit:junit:${Versions.junit}"
+    val assertj_core = "org.assertj:assertj-core:${Versions.assertj_core}"
+    val mockito_kotlin = "com.nhaarman:mockito-kotlin:${Versions.mockito_kotlin}"
+    val lifecycle_testing = "android.arch.core:core-testing:${Versions.lifecycle}"
+}
