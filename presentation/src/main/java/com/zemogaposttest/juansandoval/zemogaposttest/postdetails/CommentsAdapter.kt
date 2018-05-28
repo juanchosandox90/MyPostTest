f308f533276b13b6481c32780a1c92b9752e5896
Juan Camilo Sandoval Devia <juansandoval@rccl.com>
2018-05-27 20:55:26 -0500

    Fixed a bug in the avatar comments section.

--- presentation/src/main/java/com/zemogaposttest/juansandoval/zemogaposttest/postdetails/CommentsAdapter.kt
+++ presentation/src/main/java/com/zemogaposttest/juansandoval/zemogaposttest/postdetails/CommentsAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import com.zemogaposttest.juansandoval.zemogaposttest.R
 import com.zemogaposttest.juansandoval.zemogaposttest.inflate
+import com.zemogaposttest.juansandoval.zemogaposttest.loadAvatar
 import com.zemogaposttest.juansandoval.zemogaposttest.model.CommentItem
 import kotlinx.android.synthetic.main.include_user_info_small.view.*
 import kotlinx.android.synthetic.main.item_list_comment.view.*
@@ -24,7 +25,7 @@ class CommentsAdapter @Inject constructor() : RecyclerView.Adapter<CommentsAdapt
     inner class ViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(parent.inflate(R.layout.item_list_comment)) {
 
         fun bind(item: CommentItem) {
-            //itemView.userAvatar.loadAvatar(item.email)
+            itemView.userAvatar.loadAvatar(item.email)
             itemView.userName.text = item.name.capitalize()
             itemView.commentBody.text = item.body.capitalize()
         }
